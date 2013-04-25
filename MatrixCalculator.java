cb22886b2a1342a7f538cb28fe30000bef506b6a
lassebe <lassebe@kth.se>
2013-04-26 00:34:31 +0300

    Create MatrixCalculator.java
    Made a new class that will contain all the operations that can be performed on one or more matrices

---
+++ MatrixCalculator.java
@@ -0,0 +1,30 @@
+
+public class MatrixCalculator {
+
+  
+	/**
+	 * Adds two Matrices together and returns the sum as a new Matrix.
+	 * @param B Matrix to be added to (this)
+	 */
+	public Matrix addMatrix(Matrix A, Matrix B){
+		if( B.getNumRows() != A.getNumRows() || A.getNumCols() != B.getNumCols() ){
+			throw new IllegalArgumentException("Matriserna har olika storlek.");
+		}
+		int n = A.getNumRows();
+		int m = A.getNumCols();
+		
+		double[][] sum  = new double [n][];
+		for(int i=0; i < n; i++){
+			sum[i] = new double [m];
+		}
+		
+		for(int i=0; i < n; i++){
+			for(int j=0; j < m; j++){
+				sum[i][j] = A.getElement(i, j) + B.getElement(i, j);
+			}
+		}
+		
+		return new Matrix(sum);
+
+	}
+}
