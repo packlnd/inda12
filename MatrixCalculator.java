28808f14125e7c16b3de3288937a4752317c07fe
lassebe <lassebe@kth.se>
2013-04-26 21:46:18 +0300

    Added matrix multiplication
    Considering renaming methods.

--- MatrixCalculator.java
+++ MatrixCalculator.java
@@ -4,11 +4,12 @@ public class MatrixCalculator {
 	
 	/**
 	 * Adds two Matrices together and returns the sum as a new Matrix.
-	 * @param B Matrix to be added to (this)
+	 * @param A Matrix
+	 * @param B Matrix
 	 */
 	public Matrix add(Matrix A, Matrix B){
-		if( B.getNumRows() != A.getNumRows() || A.getNumCols() != B.getNumCols() ){
-			throw new IllegalArgumentException("Matriserna har olika storlek.");
+		if( A.getNumRows() != B.getNumRows() || A.getNumCols() != B.getNumCols() ){
+			throw new IllegalArgumentException("Matrices differ in size.");
 		}
 		int n = A.getNumRows();
 		int m = A.getNumCols();
@@ -27,4 +28,39 @@ public class MatrixCalculator {
 		return new Matrix(sum);
 
 	}
+	
+	/**
+	 * Multiplies two Matrices together and returns the sum as a new Matrix.
+	 * @param A Matrix
+	 * @param B Matrix
+	 */
+	public Matrix multiply(Matrix A, Matrix B){
+		if( A.getNumCols() != B.getNumRows()  ){
+			throw new IllegalArgumentException("Number of rows in A is not equal to the number of columns in B.");
+		}
+		
+		
+		int n = A.getNumCols();
+		int m = A.getNumRows();
+		int p = B.getNumCols();
+		
+		/*double[][] product  = new double [m][];
+		for(int i=0; i < m; i++){
+			product[i] = new double [p];
+		}*/
+		double[][] product = new double[m][p];
+		
+		for(int i=0; i < m; i++){
+			for(int j=0; j < p; j++){
+				for(int k=0; k < n; k++){
+					product[i][j] += A.getElement(i, k) * B.getElement(k, j);			
+				}
+			}
+		}
+		
+		return new Matrix(product);
+
+	}
+	
+	
 }
