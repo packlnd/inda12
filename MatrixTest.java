09bb4605ba73fe04a0ecf379dcecb8256207a719
lassebe <lassebe@kth.se>
2013-04-26 21:45:04 +0300

    Added tests for matrix multiplication and Matrix.toString()

--- MatrixTest.java
+++ MatrixTest.java
@@ -28,9 +28,7 @@ public class MatrixTest{
     @Before
     public void setUp() {
     	m0 = new Matrix(4,4);
-    	elements = new double[4][];
-    	for(int i=0; i < 4; i++)
-    		elements[i] = new double[4];
+    	elements = new double[4][4];
     	
     	for(int i=0; i < 4; i++){
     		for(int j=0; j < 4; j++){
@@ -39,6 +37,7 @@ public class MatrixTest{
     	}
     	
     	m4x4 = new Matrix(elements);
+ 
     }
 
     /**
@@ -96,6 +95,10 @@ public class MatrixTest{
     	
     }
     
+    /**
+     * Tests the add(Matrix, Matrix) function.
+     * 
+     */
     @Test
     public void TestAddMatrix(){
     	Matrix m = calc.add(m0, m4x4);
@@ -125,4 +128,92 @@ public class MatrixTest{
     	
     }
     
+    /*
+     * Tests the multiply(Matrix, Matrix).
+     */
+    @Test
+    public void TestMultiplyMatrix(){
+    	Matrix m = calc.multiply(m0, m4x4);
+    	
+    	//Tests 4x4 matrix
+    	for(int i = 0; i < 4; i++){
+    		for(int j = 0; j < 4; j++){
+    			assertEquals(0, m.getElement(i, j), 0.001);
+    		}
+    	}
+    	
+    	//m += m4x4
+    	m = calc.add(m4x4, m);
+    	//m *= m
+    	m = calc.multiply(m,m);
+
+    	assertEquals(m.getElement(0, 0), 14, 0.001);
+    	assertEquals(m.getElement(0, 1), 20, 0.001);
+    	assertEquals(m.getElement(0, 2), 26, 0.001);
+    	assertEquals(m.getElement(0, 3), 32, 0.001);
+
+    	assertEquals(m.getElement(1, 0), 20, 0.001);
+    	assertEquals(m.getElement(1, 1), 30, 0.001);
+    	assertEquals(m.getElement(1, 2), 40, 0.001);
+    	assertEquals(m.getElement(1, 3), 50, 0.001);
+
+    	assertEquals(m.getElement(2, 0), 26, 0.001);
+    	assertEquals(m.getElement(2, 1), 40, 0.001);
+    	assertEquals(m.getElement(2, 2), 54, 0.001);
+    	assertEquals(m.getElement(2, 3), 68, 0.001);
+    	
+    	assertEquals(m.getElement(3, 0), 32, 0.001);
+    	assertEquals(m.getElement(3, 1), 50, 0.001);
+    	assertEquals(m.getElement(3, 2), 68, 0.001);
+    	assertEquals(m.getElement(3, 3), 86, 0.001);
+    	
+    	//Create row matrix filled with 2's
+    	Matrix m4x1 = new Matrix(4, 1);	
+    	m4x1.addElement(0, 0, 2.0);
+    	m4x1.addElement(1, 0, 2.0);
+    	m4x1.addElement(2, 0, 2.0);
+    	m4x1.addElement(3, 0, 2.0);
+    	
+    	//Create col matrix filled with 2's
+    	Matrix m1x4 = new Matrix(1, 4);
+    	m1x4.addElement(0, 0, 2.0);
+    	m1x4.addElement(0, 1, 2.0);
+    	m1x4.addElement(0, 2, 2.0);
+    	m1x4.addElement(0, 3, 2.0);
+    	
+    	//Tests that A*B != BA
+    	m = calc.multiply(m1x4, m4x1);
+    	assertEquals(m.getElement(0,0), 16, 0.001);
+    	
+    	m = calc.multiply(m4x1, m1x4);
+    	assertEquals(m.getElement(0,0), 4, 0.001);
+    	assertEquals(m.getElement(1,1), 4, 0.001);
+    	assertEquals(m.getElement(2,2), 4, 0.001);
+    	assertEquals(m.getElement(3,3), 4, 0.001);
+    	
+    	
+    	//Tests that matrix multiplication doesn't work when 
+    	//number of cols in A != number of rows in B
+    	try{
+    		calc.multiply(m1x4, m1x4);
+    		fail();
+    	}catch (IllegalArgumentException e){
+    	}
+    	
+    }
+    
+    
+    /**
+     * Tests the toString method works properly.
+     */
+    public void TestToString(){
+    	//4x4 matrix with all entries = 0
+    	assertEquals("[[0.0 0.0 0.0 0.0][0.0 0.0 0.0 0.0][0.0 0.0 0.0 0.0][0.0 0.0 0.0 0.0]]", m0.toString());
+    	//4x4 matrix where element at [i,j] = i+j
+    	assertEquals("[[0.0 1.0 2.0 3.0][1.0 2.0 3.0 4.0][2.0 3.0 4.0 5.0][3.0 4.0 5.0 6.0]]", m4x4.toString());
+    	
+    	Matrix m = new Matrix(1,1);
+    	m.addElement(1, 1, 7);
+    	assertEquals("[[7]]", m.toString());
+    }
 }
