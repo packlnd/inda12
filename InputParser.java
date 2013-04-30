5ff7835198d112e96256f242ce9d848f6a1957fd
packlnd <patrikackland@gmail.com>
2013-04-30 18:16:43 +0200

    Parser hanterar ensam matris och fel input.

--- InputParser.java
+++ InputParser.java
@@ -1,10 +1,52 @@
+import java.util.HashSet;
 /**
  * Parses commands from View and turns it into commands 
  * for the MatrixOperations class.
  */
 public class InputParser {
+
+	private HashSet<String> keywords = new HashSet<String>();
+
+	/**
+	 * Class constructor.
+	 */
+	public InputParser() {
+		keywords.add("invert");
+		keywords.add("multiply");
+		keywords.add("determinant");
+		keywords.add("add");
+	}
+
+	/**
+	 * Takes a string and turns it into matrix operations.
+	 */
 	public String parseData(String input) {
-		// regex
-		return "output";
+		String[] rows = input.split("(] \\[)|(\\[\\[)|(]])");
+		String keyword = "";
+		String rowOfNumbers = "(\\d)(\\s\\d)*";
+
+		// Has keyword
+		if (keywords.contains(rows[0].trim())) {
+			keyword = rows[0].trim();
+
+		// Only matrix
+		} else if (rows[0].trim().matches("")) {
+			for (int i = 1; i < rows.length; i++) {
+				if (rows[i].trim().matches(rowOfNumbers)) {
+					continue;
+				}
+				return "I do not know what you mean!";
+			}
+			return input;
+
+		// Something wrong with input
+		} else {
+			return "I do not know what you mean!";
+		}
+
+		for (int i = 1; i < rows.length; i++) {
+			// Matrix operations
+		}
+		return "Correct input";
 	}
 }
