cef1fc8776589f1b05cd12115f7f550a7a054171
lassebe <lassebe@kth.se>
2013-05-10 17:19:10 +0300

    Exception for the multiply-method

---
+++ NonMultipliableMatrices.java
@@ -0,0 +1,10 @@
+
+public class NonMultipliableMatrices extends Exception {
+  private static final long serialVersionUID = 1L;
+
+	public NonMultipliableMatrices() {}
+	
+	public NonMultipliableMatrices(String message){
+		super(message);
+	}
+}
