004aff47bec724f04ba5956ae2f918ab0a441a6f
lassebe <lassebe@kth.se>
2013-05-10 17:19:47 +0300

    Exception for invert/determinant

---
+++ NonSquareMatrixException.java
@@ -0,0 +1,10 @@
+
+public class NonSquareMatrixException extends Exception {
+  private static final long serialVersionUID = 1L;
+
+	public NonSquareMatrixException() {}
+	
+	public NonSquareMatrixException(String message){
+		super(message);
+	}
+}
