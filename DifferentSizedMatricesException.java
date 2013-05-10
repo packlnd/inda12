0eb76e3b6cec586ae43ec8cb5ff8b93309b53d2f
lassebe <lassebe@kth.se>
2013-05-10 16:18:38 +0200

    Exception for the add-method

---
+++ DifferentSizedMatricesException.java
@@ -0,0 +1,10 @@
+
+public class DifferentSizedMatricesException extends Exception {
+  private static final long serialVersionUID = 1L;
+
+	public DifferentSizedMatricesException() {}
+	
+	public DifferentSizedMatricesException(String message){
+		super(message);
+	}
+}
