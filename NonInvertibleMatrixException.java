398c7f55c353c1601d6475d0fbed089a7366544f
lassebe <lassebe@kth.se>
2013-05-10 17:20:11 +0300

    Exception for the invert-method

---
+++ NonInvertibleMatrixException.java
@@ -0,0 +1,10 @@
+
+public class NonInvertibleMatrixException extends Exception {
+  private static final long serialVersionUID = 1L;
+
+	public NonInvertibleMatrixException() {}
+	
+	public NonInvertibleMatrixException(String message){
+		super(message);
+	}
+}
